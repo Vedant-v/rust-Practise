a500bbe1a29139c1430811f6b876ceecbab22451
vedant <vedantkulkarni.20.000@gmail.com>
2025-11-29 09:45:26 +0000

    Initial commit of guessing game

---
+++ gessing_game/src/main.rs
@@ -0,0 +1,26 @@
+use std::{io,cmp::Ordering};
+use rand;
+fn main() {
+    println!("wellcome to gessing game");
+    let secrite =rand::random_range(1..=100);
+    println!("the secrite value is {secrite}");
+    loop{
+        println!("Please enter the number:");
+        let mut gesses = String::new();
+    
+        io::stdin()
+            .read_line(&mut gesses)
+            .expect("cant read your input");
+        let gesses: u32 =gesses.trim().parse().expect("please type  a number!");
+        println!("you gessed : {gesses}");
+    
+        match gesses.cmp(&secrite){
+            Ordering::Less => println!("too small!"),
+            Ordering::Greater => println!("too big"),
+            Ordering::Equal => {
+                println!("you win");
+                break;
+            }
+        }
+    }
+}
